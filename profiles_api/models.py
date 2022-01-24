967d24e72778e2e9846a7c8de76a25c665f079ad
esgaelramos <esgaelramos@gmail.com>
2022-01-24 17:32:54 -0600

    Before we add to requeriments.txt a lib for psql. Now create a user model

--- profiles_api/models.py
+++ profiles_api/models.py
@@ -1,3 +1,32 @@
 from django.db import models
 
+from django.contrib.auth.models import AbstractBaseUser
+from django.contrib.auth.models import PermissionsMixin
+
 # Create your models here.
+
+#We re-write the model default user, but we got one login with email and not only user:
+
+class UserProfile(AbstractBaseUser, PermissionsMixin):
+    """" MODEL DATABASE FOR USERS IN SYSTEM """
+    email = models.EmailField(max_length=255, unique=True)
+    name = models.CharField(max_length=20)
+    #When user 'delete' his profile has a 'backup', because we only not show his profile, but never delete
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+
+    objects = UserProfileManager()
+    
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['name']
+
+    def get_full_name(self):
+        return self.name
+    
+    def get_short_name(self):
+        return self.name
+
+    def __str__(self):
+        """ RETURN STRING LIKE USER"""
+        return self.email
+    
