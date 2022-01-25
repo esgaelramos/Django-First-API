f26ce725146847b841db770d98f5e5d3cf05cce4
esgaelramos <esgaelramos@gmail.com>
2022-01-25 17:47:18 -0600

    Create model superuser

--- profiles_api/models.py
+++ profiles_api/models.py
@@ -2,8 +2,35 @@ from django.db import models
 
 from django.contrib.auth.models import AbstractBaseUser
 from django.contrib.auth.models import PermissionsMixin
+from django.contrib.auth.models import BaseUserManager
 
 # Create your models here.
+
+class UserProfileManager(BaseUserManager):
+    """ MANAGER FOR MANAGE PROFILES USER """
+
+    def create_user(self, email, name, password=None):
+        """ CREATE NEW USER PROFILE """
+        if not email:
+            raise ValueError('User need email')
+        
+        email = self.normalize_email(email)
+        user = self.model(email=email, name=name)
+
+        user.set_password(password)
+        user.save(using=self._db)
+
+        return user
+
+    def create_superuser(self, email, name, password):
+        user = self.create_user(email, name, password)
+
+        #is_superuser is default django with PermissionsMixin
+        user.is_superuser = True
+        user.is_staff = True
+        user.save(using=self._db)
+
+        return user        
 
 #We re-write the model default user, but we got one login with email and not only user:
 
