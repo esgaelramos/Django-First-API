c78e05a94ed0f6c3ee5706c4d8b5c91b5d4354df
esgaelramos <esgaelramos@gmail.com>
2022-01-28 13:53:53 -0600

    Import and create API view

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
 
+from rest_framework.views import APIView
+from rest_framework.response import Response
+
 # Create your views here.
+
+class HelloAPIView(APIView):
+    """ API VIEW OF TEST """
+    
+    def get(self, request, format=None):
+        """ RETURN LIST PROPIETIS APIVIEW """
+        an_apiview = [
+            'WE USE METHODS HTTP LIKE FUNCTIONS (GET, POST, PATCH, PUT, DELETE)',
+            'LIKE A TRADITIONAL VIEW',
+            'BETTER CONTROL ABOUT APP LOGIC',
+            'MANUAL MAPPING URLS'
+        ]
+        #Always we need a response in format JSON = dictionary
+        return ResourceWarning({'message': 'Hello', 'an_apiview': an_apiview})
