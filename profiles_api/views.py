0675ed1a8859e0f2266f34b0399dd9c45d05c8a4
esgaelramos <esgaelramos@gmail.com>
2022-02-01 16:10:55 -0600

    Create class serializer and import

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -2,11 +2,15 @@ from django.shortcuts import render
 
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import status
+
+from profiles_api import serializers
 
 # Create your views here.
 
 class HelloAPIView(APIView):
     """ API VIEW OF TEST """
+    serializer_class = serializers.HelloSerializer
     
     def get(self, request, format=None):
         """ RETURN LIST PROPIETIS APIVIEW """
@@ -18,3 +22,5 @@ class HelloAPIView(APIView):
         ]
         #Always we need a response in format JSON = dictionary
         return Response(({'message': 'Hello', 'an_apiview': an_apiview}))
+
+
